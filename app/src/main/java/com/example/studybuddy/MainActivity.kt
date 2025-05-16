95c1153681725b79be2d7ab5fd64fe5e3e0dea12
kunstaytrue <st10487104@rcconnect.edu.za>
2025-05-16 10:39:44 +0200

    addded text for the welcome screen

--- app/src/main/java/com/example/studybuddy/MainActivity.kt
+++ app/src/main/java/com/example/studybuddy/MainActivity.kt
@@ -6,8 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,6 +20,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.studybuddy.ui.theme.StudybuddyTheme
 
 class MainActivity : ComponentActivity() {
@@ -42,11 +47,14 @@ class MainActivity : ComponentActivity() {
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center
             ) {
+                if (showWelcomeScreen.value) {
+                    Text(text = "Welcome to the MY5thtry App!", style = MaterialTheme.typography.headlineLarge)
+                    Spacer(modifier = Modifier.height(24.dp))
 
-
-
-
-
+                    Button(onClick = { showWelcomeScreen.value = false }) {
+                        Text(text = "Start")
+                    }
+                    
             }
     }
 }
