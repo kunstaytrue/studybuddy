9121ca6ed401b24be7eacccba626e473202fb524
kunstaytrue <st10487104@rcconnect.edu.za>
2025-05-16 10:31:18 +0200

    added the array of questions and answers

--- app/src/main/java/com/example/studybuddy/MainActivity.kt
+++ app/src/main/java/com/example/studybuddy/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.studybuddy.ui.theme.StudybuddyTheme
@@ -25,7 +28,26 @@ class MainActivity : ComponentActivity() {
             var score = remember { mutableStateOf(0) }
             var showScoreScreen = remember { mutableStateOf(false) }
 
-        }
+            val questions = arrayOf(
+                "The Great Wall of China is visible from space.",
+                "The Titanic sank on its maiden voyage.",
+                "Napoleon Bonaparte was extremely short.",
+                "The Berlin Wall fell in 1989.",
+                "The United States declared independence from France."
+            )
+            val answers = arrayOf(false, true, false, true, false)
+
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+
+
+
+
+
+            }
     }
 }
 
