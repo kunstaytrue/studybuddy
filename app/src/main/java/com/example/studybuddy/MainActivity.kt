48bc14df2b0feb6454788ce895c66836d7ea4641
kunstaytrue <st10487104@rcconnect.edu.za>
2025-05-16 15:33:41 +0200

    so i added an image for the background

--- app/src/main/java/com/example/studybuddy/MainActivity.kt
+++ app/src/main/java/com/example/studybuddy/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -19,10 +20,20 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.studybuddy.ui.theme.StudybuddyTheme
 import kotlin.system.exitProcess
+import androidx.compose.foundation.Image
+import androidx.compose.ui.res.painterResource
+import androidx.compose.foundation.Image
+import androidx.compose.ui.res.painterResource
+
+
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,6 +47,15 @@ class MainActivity : ComponentActivity() {
             val feedback = remember { mutableStateOf("") }
             val showReviewScreen = remember { mutableStateOf(false) }
 
+
+            Box(modifier = Modifier.fillMaxSize()) {
+                Image(
+                    painter = painterResource(id=R.drawable.a6_flashcards_blauw_gelinieerd_voorzijde),
+                    contentDescription = null ,
+                    contentScale= ContentScale.FillBounds,
+                    modifier= Modifier.fillMaxSize()
+                )
+            }
             val questions = arrayOf(
                 "The Great Wall of China is visible from space.",
                 "The Titanic sank on its maiden voyage.",
