6ece4a4627aac394d9bbda65929ce4b29cd0bc2a
kunstaytrue <st10487104@rcconnect.edu.za>
2025-05-16 10:28:05 +0200

    added variables for the different screens , user score and question index

--- app/src/main/java/com/example/studybuddy/MainActivity.kt
+++ app/src/main/java/com/example/studybuddy/MainActivity.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.studybuddy.ui.theme.StudybuddyTheme
@@ -18,30 +20,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            StudybuddyTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            var showWelcomeScreen = remember { mutableStateOf(true) }
+            var currentQuestionIndex = remember { mutableStateOf(0) }
+            var score = remember { mutableStateOf(0) }
+            var showScoreScreen = remember { mutableStateOf(false) }
+
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    StudybuddyTheme {
-        Greeting("Android")
-    }
-}
